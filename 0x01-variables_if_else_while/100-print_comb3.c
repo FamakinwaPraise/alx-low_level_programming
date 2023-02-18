7fb2b937173346a268eaeb89dff8d6eda3ea8401
FamakinwaPraise <Praisealabicomfort@gmail.com>
2023-02-17 17:28:04 -0800

    Comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (Succes)
+ * Return: always 0
  */
-
 int main(void)
 {
-	int n;
-	int i = 0;
+	int c, c_two;
 
-	for (n = 0 ; n < 100 ; n++)
+	for (c = 48; c <= 56; c++)
 	{
-		putchar(n / 10 + '0');
-		putchar(i + '0');
-		if (n < 99)
+		for (c_two = c + 1; c_two <= 57; c_two++)
 		{
-			putchar(',');
-			putchar(32);
-		}
-		i++;
-		if (i > 9)
-		{
-			i = 0;
+			putchar(c);
+			putchar(c_two);
+			if (c != 56 || c_two != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 	}
-	putchar('\n');
+
+	putchar(10);
 	return (0);
 }
